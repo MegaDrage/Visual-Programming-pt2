e373bb291ef3889051a688669c338d59d983fe54
MegaDrage <dragonpsui@gmail.com>
2024-09-08 17:00:15 +0700

    base project

--- flake.nix
+++ flake.nix
@@ -1,27 +1,31 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
 
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
-      packages.x86_64-linux.task1 = pkgs.qt6Packages.callPackage ./task1 { };
+
+      packages.x86_64-linux.default = pkgs.qt6Packages.callPackage ./task1 { };
+      packages.x86_64-linux.task2 = pkgs.qt6Packages.callPackage ./task1 { };
+
       devShells.x86_64-linux.default = pkgs.mkShell {
+
         inputsFrom = [ self.packages.x86_64-linux.default ];
         buildInputs = with pkgs; [
           gdb
           qtcreator
 
-          # this is for the shellhook portion
           qt6.wrapQtAppsHook
           makeWrapper
           bashInteractive
         ];
-        # set the environment variables that Qt apps expect
+
         shellHook = ''
           bashdir=$(mktemp -d)
           makeWrapper "$(type -p bash)" "$bashdir/bash" "''${qtWrapperArgs[@]}"
-          exec "$bashdir/bash"
+          nohup qtcreator &
         '';
+        # exec "$bashdir/bash"
       };
     };
 }
